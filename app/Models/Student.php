3546babb9ee4a9ae99ed170e5409ce04e597fe11
Md-Thanveer <thanveerami612@gmail.com>
2024-12-28 18:58:54 +0530

    group sections that the student relationship added
    In models Student.php i updated the
    
    public function groupSections()

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -17,4 +17,10 @@ class Student extends Model
         'gender',
         'dob'
     ];
+
+    public function groupSections()
+    {
+        return $this->belongsToMany(GroupSection::class, 'student_group_section', 'student_id', 'group_section_id')
+                    ->withTimestamps();
+    }
 }

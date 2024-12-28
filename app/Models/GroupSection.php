ca467809f6ff22b84fcf8a9ec563c94e51342776
Md-Thanveer <thanveerami612@gmail.com>
2024-12-28 19:31:01 +0530

    GroupSection relationship fixed
    changed the
      protected $table = 'group_section';
     protected $fillable = ['group_id', 'section_id'];
        public function section()
        {
            return $this->belongsTo(Section::class, 'section_id');
        }
    
        /**
         * Relationship with Group model
         */
        public function group()
        {
            return $this->belongsTo(Group::class, 'group_id');
        }

--- app/Models/GroupSection.php
+++ app/Models/GroupSection.php
@@ -1,11 +1,18 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class GroupSection extends Model
 {
     use HasFactory;
+
+    protected $table = 'group_section';
+
+    protected $fillable = ['group_id', 'section_id'];
+
     /**
      * The students that belong to the group section.
      */
@@ -13,5 +20,21 @@ class GroupSection extends Model
     {
         return $this->belongsToMany(Student::class, 'student_group_section', 'group_section_id', 'student_id')
                     ->withTimestamps();
+    }
+
+    /**
+     * Relationship with Section model
+     */
+    public function section()
+    {
+        return $this->belongsTo(Section::class, 'section_id');
+    }
+
+    /**
+     * Relationship with Group model
+     */
+    public function group()
+    {
+        return $this->belongsTo(Group::class, 'group_id');
     }
 }
--- app/Models/Section.php
+++ app/Models/Section.php
@@ -17,11 +17,21 @@ class Section extends Model
     protected $fillable = [
         'name'
     ];
+
     /**
      * The groups that belong to the section.
      */
     public function groups()
     {
         return $this->belongsToMany(Group::class, 'group_section', 'section_id', 'group_id');
+    }
+
+    
+    /**
+     * Relationship with GroupSection model
+     */
+    public function groupSections()
+    {
+        return $this->hasMany(GroupSection::class, 'section_id');
     }
 }

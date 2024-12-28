cbaaa206de55cf0d1c3cb39b88e240b3ac19c176
Md-Thanveer <thanveerami612@gmail.com>
2024-12-28 19:15:17 +0530

    Create SectionSeeder.php
    php artisan make:seed SectionSeeder

--- database/seeders/SectionSeeder.php
+++ database/seeders/SectionSeeder.php
@@ -2,27 +2,38 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 use App\Models\Section;
 
 class SectionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
         $sections = [
-            ['name' => 'A'],
-            ['name' => 'B'],
-            ['name' => 'C'],
-            ['name' => 'D'],
+            [
+                'name' => 'A'                
+            ],
+            [
+                'name' => 'B'
+            ],
+            [
+                'name' => 'C'
+            ],
+            [
+                'name' => 'D'
+            ],
         ];
+        
+        // Section::insert($sections);
 
-        foreach ($sections as $section) {
-            Section::create($section);
+        foreach ($sections as $row) 
+        {
+            Section::create($row);
         }
     }
 }

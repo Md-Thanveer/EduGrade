fc8840cac9ab3a7f8092677dc9d4a4d0b8bcf83e
Md-Thanveer <thanveerami612@gmail.com>
2024-12-28 19:32:26 +0530

    numberic typo fixed to numeric
    numberic typo fixed to numeric

--- database/migrations/2024_12_28_081626_create_groups_table.php
+++ database/migrations/2024_12_28_081626_create_groups_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('numberic');
+            $table->string('numeric');
             $table->timestamps();
         });
     }

6a1a4e0a04dff54ea510fac67a281d4589d7eadf
Md-Thanveer <thanveerami612@gmail.com>
2024-12-28 19:12:48 +0530

    Create GroupSeeder.php
    php artisan make:seed GroupSeeder

--- database/seeders/GroupSeeder.php
+++ database/seeders/GroupSeeder.php
@@ -2,27 +2,38 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 use App\Models\Group;
 
 class GroupSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
         $groups = [
-            ['name' => 'Computer Science', 'numberic' => 1],
-            ['name' => 'Bio-Maths', 'numberic' => 2],
-            ['name' => 'Pure-Science', 'numberic' => 3],
-            ['name' => 'Commerce', 'numberic' => 4],
+            [
+                'name' => 'First',
+                'numberic' => '1'
+            ],
+            [
+                'name' => 'Second',
+                'numberic' => '2'
+            ],
+            [
+                'name' => 'Third',
+                'numberic' => '3'
+            ],
         ];
+        
+        // Group::insert($groups);
 
-        foreach ($groups as $group) {
-            Group::create($group);
+        foreach ($groups as $row) 
+        {
+            Group::create($row);
         }
     }
 }

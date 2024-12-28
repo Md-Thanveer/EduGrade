a532bd576cd218d41f088314534113ddc491c6ec
Md-Thanveer <thanveerami612@gmail.com>
2024-12-28 18:53:20 +0530

    sections that belong to the group relationship added
    for Model Group.php i added the code
     public function sections()
        {
            return $this->belongsToMany(Section::class, 'group_section', 'group_id', 'section_id');
        }

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -1,10 +1,14 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 class Group extends Model
 {
     use HasFactory;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -14,4 +18,12 @@ class Group extends Model
         'name',
         'numberic'
     ];
+
+    /**
+     * The sections that belong to the group.
+     */
+    public function sections()
+    {
+        return $this->belongsToMany(Section::class, 'group_section', 'group_id', 'section_id');
+    }
 }
